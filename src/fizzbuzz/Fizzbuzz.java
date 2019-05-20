331cf349600bba158049fbbe0e6df7a4d2908d12
Tiago Lisboa <tiago.caio.ol@gmail.com>
2019-05-20 10:12:13 -0300

    teste numero = 1

---
+++ src/fizzbuzz/Fizzbuzz.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fizzbuzz;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ *
+ * @author aluno
+ */
+public class Fizzbuzz {
+
+    /**
+     * @param i the number to be checked
+     */
+    public String Answer(int i) {
+        throw new NotImplementedException();
+    }
+    
+}
---
+++ test/fizzbuzz/FizzbuzzTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fizzbuzz;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author aluno
+ */
+public class FizzbuzzTest {
+    
+    public FizzbuzzTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of Answer method, of class Fizzbuzz.
+     */
+    @Test
+    public void testAnswer() {
+        System.out.println("Answer");
+        int i = 1;
+        Fizzbuzz instance = new Fizzbuzz();
+        String expResult = "1";
+        String result = instance.Answer(i);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+    
+}
